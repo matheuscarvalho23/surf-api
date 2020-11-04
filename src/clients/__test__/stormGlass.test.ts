0ec31d7f433db1f957ce266d2d521208e83e7b57
Matheus Carvalho <matheus.santos.hcs@gmail.com>
2020-11-04 09:00:55 -0300

    Update

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,13 +1,20 @@
+import axios from 'axios';
 import { StormGlass } from '@src/clients/stormGlass';
+import stormGlass3Hours from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlass3HoursNormalized from '@test/fixtures/stormglass_normalized_3_hours.json';
+
+jest.mock('axios');
 
 describe('StormGlass client', () => {
   it('Should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    const stormGlass = new StormGlass();
-    const response = await stormGlass.fetchPoints(lat, lng);
+    axios.get = jest.fn().mockResolvedValue(stormGlass3Hours);
 
-    expect(response).toEqual({});
+    const stormGlass = new StormGlass(axios);
+    const response   = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlass3HoursNormalized);
   });
 });
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,11 +1,27 @@
 import { AxiosStatic } from 'axios';
 
+export interface StormGlassSource {
+  [key: string]: number;
+}
+
+export interface StormGlassPoint {
+  readonly time: string;
+  readonly waveHeight: StormGlassSource;
+}
+export interface StormGlassResponse {
+
+}
+
+
 export class StormGlass {
+  readonly stormGlassApiParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassApiSource = 'noaa';
+
   constructor(protected request: AxiosStatic) {}
 
   public async fetchPoints(lat: number, lng: number): Promise<{}> {
     return this.request.get(
-      `https://api.stormglass.io/v2/weather/point?params=swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed&source=noaa&lat=${lat}lng=${lng}`
+      `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}&lat=${lat}lng=${lng}`
     );
   }
 }

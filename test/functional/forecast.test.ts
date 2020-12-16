872d41ec14dc75d4ccaee5e9e3e01e72e1dd5f3c
Matheus Carvalho <matheus.santos.hcs@gmail.com>
2020-12-16 10:26:59 -0300

    Changes

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,5 +1,39 @@
+import { Beach, BeachPosition } from '@src/models/beach';
+import nock from 'nock';
+import stormGlassWeather3HoursFixture from '../fixtures/stormglass_weather_3_hours.json';
+// import apiForecastResponse1BeachFixture from '../fixtures/api_forecast_response_1_beach.json';
+
 describe('Beach forecast functional tests', () => {
+  beforeEach(async () => {
+    await Beach.deleteMany({});
+    const defaultBeach = {
+      lat: -33.792726,
+      lng: 151.289824,
+      name: 'Manly',
+      position: BeachPosition.E,
+    };
+
+    const beach = new Beach(defaultBeach);
+    await beach.save();
+  });
+
   it('should return a forecast with just a few times.', async () => {
+    nock('https://api.stormglass.io:443', {
+      encodedQueryParams: true,
+      reqheaders: {
+        Authorization: (): boolean => true,
+      },
+    })
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat: '-33.792726',
+        lng: '151.289824',
+        params: /(.*)/,
+        source: 'noaa',
+      })
+      .reply(200, stormGlassWeather3HoursFixture);
+
     const { body, status } = await global.testRequest.get('/forecast');
     expect(status).toBe(200);
     expect(body).toEqual([
